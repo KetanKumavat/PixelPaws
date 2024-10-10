905a6286f9563fa090549bf449d42d6b5a2c7029
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-10-10 17:24:09 +0530

    slight changes

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://pixelpaws-wp-project.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
